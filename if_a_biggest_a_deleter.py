1ec48e631dbb3e1a9bd9c036af8fb7dafe0d7e2c
D.Dunaev <doonay81@gmail.com>
2020-12-29 12:34:11 +0300

    renamer

--- if_a_biggest_a_deleter.py
+++ if_a_biggest_a_deleter.py
@@ -18,12 +18,36 @@ if __name__ == "__main__":
     a_bigest = 0
     list_of_delete = []
 
-    index = 0
+    index1 = 0
+    index2 = 0
+    max = 0
+    List_just_a = []
+    list_max_a = []
     for f in list_files:
-    #если попался [an], где n больший, чем a_bigest, в a_bigest записываем новый номер и удаляем файл с предыдущим a
+    #пока кладем в отдельную папку только файлы без обновления вообще
         if (f.find('[a')!=-1):
-            index = f.rfind('[a]') + 3
-            print(index)
+            index1 = f.find('[a')
+            index2 = f.find('.tap')
 
+            if f[index1 + 2].isdigit() and f[index1 + 3].isdigit():
+                if max < int(f[index1 + 2] + f[index1 + 3]):
+                    max = int(f[index1 + 2] + f[index1 + 3])
 
+            elif f[index1 + 2].isdigit():
+                if max < int(f[index1 + 2]):
+                    max = int(f[index1 + 2])
+            else:
+                List_just_a.append(f)
 
+                #print(f[0:index1] + '[a' + str(max) + ']' + f[index2:len(f)])
+        else:
+            list_of_delete.append(f)
+    try:
+        os.mkdir('dont_a_files')
+    except(FileExistsError):
+        print('Всё нормально, просто такая папка уже есть. Копирую в неё')
+
+    # рекурсивно перемещает файл или директорию (src) в другое место (dst), и возвращает место назначения.
+    for file_of_delete in list_of_delete:
+        shutil.move(file_of_delete, path_name + 'dont_a_files/')
+        print(file_of_delete, 'перемещен в папку dont_a_files')
